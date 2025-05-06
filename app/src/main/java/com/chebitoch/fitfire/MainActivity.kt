c20ee3b608c5f4eda8eb5129159a966ece97f8d0
chebitoch007 <eliphazchebitoch@gmail.com>
2025-05-06 23:19:15 +0300

    flame

--- app/src/main/java/com/chebitoch/fitfire/MainActivity.kt
+++ app/src/main/java/com/chebitoch/fitfire/MainActivity.kt
@@ -1,130 +1,31 @@
 package com.chebitoch.fitfire
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.chebitoch.fitfire.navigation.AppNavHost
 import com.chebitoch.fitfire.ui.theme.FitFireTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FitFireTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize()
-                            .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = "My Home Screen",
-                            style = MaterialTheme.typography.headlineMedium,
-                            modifier = Modifier.padding(bottom = 16.dp)
-                        )
-                    HomeScreen(
-                        name = "FitFire",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-    @Composable
-    fun HomeScreen(name: String, modifier: Modifier = Modifier) {
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-        ) {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-            ) {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(2), // 2-column grid
-                    contentPadding = PaddingValues(8.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    modifier = Modifier.padding(8.dp)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    item {
-                        Button(
-                            onClick = { /* Handle Home click */ },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(48.dp)
-                        ) {
-                            Text(text = "HOME")
-                        }
-                    }
-                    item {
-                        Button(
-                            onClick = { /* Handle Profile click */ },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(48.dp)
-                        ) {
-                            Text(text = "PROFILE")
-                        }
-                    }
-                    item {
-                        Button(
-                            onClick = { /* Handle Button 3 click */ },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(48.dp)
-                        ) {
-                            Text(text = "Button 3")
-                        }
-                    }
-                    item {
-                        Button(
-                            onClick = { /* Handle Button 4 click */ },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(48.dp)
-                        ) {
-                            Text(text = "Button 4")
-                        }
-                    }
+                    AppNavHost()
                 }
             }
         }
     }
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitFireTheme {
-        HomeScreen("FitFire")
-    }
- }
 }
--- app/src/main/java/com/chebitoch/fitfire/navigation/Routes.kt
+++ app/src/main/java/com/chebitoch/fitfire/navigation/Routes.kt
@@ -1,14 +1,14 @@
 package com.chebitoch.fitfire.navigation
 
+
+const val ROUT_SPLASH = "splash"
 const val ROUT_HOME = "home"
 const val ROUT_ABOUT = "about"
-const val ROUT_PROGRESSTRACKER = "progresstracker"
-const val ROUT_WORKOUTPLANS = "workoutplans"
 const val ROUT_PROFILE = "profile"
-const val ROUT_SPLASH = "splash"
+const val ROUT_PROGRESSTRACKER = "progress_tracker"
+const val ROUT_WORKOUTPLANS = "workout_plans"
 const val ROUT_WORKOUTDETAIL = "workout_detail"
-
-// Authentication
 const val ROUT_REGISTER = "register"
 const val ROUT_LOGIN = "login"
+
 
